6ff35a026d7bc6d4d5ebaca7b909340e006b2c6b
Vadiraj Hippargi <Vadi@leadconsultant.com>
2021-06-24 21:20:02 -0700

    Initial Commit

---
+++ NFCReader/NFCReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFCReaderApp.swift
+//  NFCReader
+//
+//  Created by Vadiraj Hippargi on 6/24/21.
+//
+
+import SwiftUI
+
+@main
+struct NFCReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
